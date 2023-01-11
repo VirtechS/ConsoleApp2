ea7d0114b2982912c03743ebbd4a3d9de45df33a
Virtech <jenia_2510@mail.ru>
2023-01-11 22:09:44 +0500

    ref

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,9 +15,9 @@ class Product
 class ProductCategory
 {
     public string Name { get; set; }
-    public Product[] Products { get; set; }
+    public List<Product> Products { get; set; }
 
-    public ProductCategory(string name, Product[] products)
+    public ProductCategory(string name, List<Product> products)
     {
         this.Name = name;
         this.Products = products;
@@ -26,68 +26,96 @@ class ProductCategory
 
 class Program
 {
+    //Enum константы категорий
+    private enum CategorySelection
+    {
+        Meat = 1,
+        Bakery = 2,
+        Beverages = 3,
+        Dessert = 4
+    }
+
+    // Метод отображения товаров
+    private static void DisplayProducts(ProductCategory productCategory)
+    {
+        Console.WriteLine("Выберите товар:");
+        for (int i = 0; i < productCategory.Products.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {productCategory.Products[i].Name} - {productCategory.Products[i].Price}");
+        }
+    }
+
+    // Метод отображения категорий и возвращения выбора
+    private static ProductCategory DisplayCategories(List<ProductCategory> productCategories)
+    {
+        Console.WriteLine("Выберите категорию:");
+        for (int i = 0; i < productCategories.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {productCategories[i].Name}");
+        }
+
+        int selectedCategory;
+        while (!int.TryParse(Console.ReadLine(), out selectedCategory)
+            || !Enum.IsDefined(typeof(CategorySelection), selectedCategory))
+        {
+            Console.WriteLine("Неизвестная категория, попробуйте еще раз:");
+        }
+
+        return productCategories[selectedCategory - 1];
+    }
+
     static void Main(string[] args)
     {
-        ProductCategory[] productCategories = new ProductCategory[] {
-            new ProductCategory("Мясо", new Product[] {
-                new Product("Колбаса", 580),
-                new Product("Пельмени", 430),
-                new Product("Фарш", 230)
+        // Создание категорий
+        var productCategories = new List<ProductCategory>
+        {
+            new ProductCategory("Мясо", new List<Product> {
+                new Product("Пельмени", 580),
+                new Product("Колбаса", 430),
+                new Product("Стейк", 230)
             }),
-            new ProductCategory("Выпечка", new Product[] {
+            new ProductCategory("Выпечка", new List<Product> {
                 new Product("Хлеб", 50),
-                new Product("Батон", 70)
+                new Product("Булка", 70)
             }),
-            new ProductCategory("Напитки", new Product[] {
+            new ProductCategory("Напитки", new List<Product> {
                 new Product("Колла", 120),
                 new Product("Вода", 100),
                 new Product("Сок", 90)
             }),
-            new ProductCategory("Десерты", new Product[] {
+            new ProductCategory("Десерты", new List<Product> {
                 new Product("Кекс", 700),
                 new Product("Печенье", 350),
-                new Product("Торт", 400)
+                new Product("Тор", 400)
             }),
         };
 
-        Product[] selectedProducts = new Product[3];
+        //Создание переменной выбора
+        var selectedProducts = new List<Product>();
 
+        //Выбор 3ех продуктов
         for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine("Выберите категорию:");
-            for (int j = 0; j < productCategories.Length; j++)
+            var selectedCategory = DisplayCategories(productCategories);
+            if (selectedCategory.Products != null)
             {
-                Console.WriteLine($"{j + 1}. {productCategories[j].Name}");
-            }
+                DisplayProducts(selectedCategory);
 
-            int selectedCategory = int.Parse(Console.ReadLine());
-            while (selectedCategory < 1 || selectedCategory > productCategories.Length)
-            {
-                Console.WriteLine("Неизвестная категория, попробуйте еще раз:");
-                selectedCategory = int.Parse(Console.ReadLine());
+                int selectedProduct;
+                while (!int.TryParse(Console.ReadLine(), out selectedProduct)
+                    || selectedProduct < 1
+                    || selectedProduct > selectedCategory.Products.Count)
+                {
+                    Console.WriteLine("Неизвестный товар, попробуйте еще раз:");
+                }
+                selectedProduct--;
+                selectedProducts.Add(selectedCategory.Products[selectedProduct]);
             }
-            selectedCategory--;
-
-            Console.WriteLine("Выберите товар:");
-            for (int j = 0; j < productCategories[selectedCategory].Products.Length; j++)
-            {
-                Console.WriteLine($"{j + 1}. {productCategories[selectedCategory].Products[j].Name} - {productCategories[selectedCategory].Products[j].Price}");
-            }
-
-            int selectedProduct = int.Parse(Console.ReadLine());
-            while (selectedProduct < 1 || selectedProduct > productCategories[selectedCategory].Products.Length)
-            {
-                Console.WriteLine("Неизвестный товар, попробуйте еще раз:");
-                selectedProduct = int.Parse(Console.ReadLine());
-            }
-            selectedProduct--;
-
-            selectedProducts[i] = productCategories[selectedCategory].Products[selectedProduct];
         }
 
         int totalPrice = 0;
         Console.WriteLine("Чек:");
-        for (int i = 0; i < selectedProducts.Length; i++)
+        for (int i = 0; i < selectedProducts.Count; i++)
         {
             Console.WriteLine(selectedProducts[i].Name + " - " + selectedProducts[i].Price);
             totalPrice += selectedProducts[i].Price;
